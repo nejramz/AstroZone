3dc544d0e92bc34d501c4b3235376a80c37d5c80
zakiraskaljic <104206844+zakiraskaljic@users.noreply.github.com>
2022-05-24 07:02:00 +0200

    Delete astrozone directory

--- astrozone/Includes/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-    <h3>Astro<span class="purple">Zone</span></h3>
-    <nav>
-        <a href="/astrozone/login_page.php" target="_blank">Log in</a>
-    </nav>
-</header>
--- astrozone/Includes/header2.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <h3>Astro<span class="purple">Zone</span></h3>
-    <nav>
-        <a href="/astrozone/articles_page.php">Articles</a>
-        <a href="/astrozone/friends_page.php">Friends</a>
-        <a href="/astrozone/logout.php">Log out</a>
-    </nav>
-</header>
