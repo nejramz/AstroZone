809721db8f596973f61307d6fc48918f8d8ec030
zakiraskaljic <104206844+zakiraskaljic@users.noreply.github.com>
2022-05-24 07:02:56 +0200

    Add files via upload

---
+++ astrozone/Includes/header2.php
@@ -0,0 +1,8 @@
+<header>
+    <h3>Astro<span class="purple">Zone</span></h3>
+    <nav>
+        <a href="\astrozone\rticles_page.php">Articles</a>
+        <a href="\astrozone\friends_page.php">Friends</a>
+        <a href="\astrozone\logout.php">Log out</a>
+    </nav>
+</header>
